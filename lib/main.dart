4a2456239656921d92a05190333a0ca66f8aa957
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-09-07 09:50:54 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:gem_kit/gem_kit_map_controller.dart';
+import 'package:gem_kit/widget/gem_kit_map.dart';
+import 'package:gem_kit/api/gem_sdksettings.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Map',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late GemMapController mapController;
+
+  final _token = 'YOUR_API_TOKEN';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> onMapCreated(GemMapController controller) async {
+    mapController = controller;
+
+    SdkSettings.setAppAuthorization(_token);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: [
+            GemMap(
+              onMapCreated: onMapCreated,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
