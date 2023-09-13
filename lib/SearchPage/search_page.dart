023720c7e05ea2f87608160589f5b998940c9707
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-09-13 12:51:36 +0300

    Create the elements from SearchPage

--- lib/SearchPage/search_page.dart
+++ lib/SearchPage/search_page.dart
@@ -49,9 +49,55 @@ class SearchPage extends StatelessWidget {
         return ListView.builder(
           itemCount: state.landmarksInfoList?.length ?? 0,
           itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text(state.landmarksInfoList![index].name!),
-            );
+            return Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: const BorderSide(
+                      color: Colors.grey,
+                      width: 2.0,
+                    ),
+                    bottom: (index + 1 == state.landmarksInfoList?.length)
+                        ? const BorderSide(
+                            color: Colors.grey,
+                            width: 2.0,
+                          )
+                        : BorderSide.none,
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Image.memory(
+                          ((state.landmarksInfoList![index].image)!),
+                          scale: 3,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                              maxWidth:
+                                  MediaQuery.of(context).size.width * 0.82),
+                          child: Text(
+                            '${state.landmarksInfoList![index].name}',
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                      ],
+                    ),
+                  ],
+                ));
           },
         );
       }),
